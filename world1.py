927fb9de6625b65d05508db77fda5eeec13e0a63
xujsthu18 <43575819+xujsthu18@users.noreply.github.com>
2020-05-08 14:39:53 +0630

    Update world1.py

--- world1.py
+++ world1.py
@@ -6,6 +6,7 @@ import plotly.express as px
 path = 'maptemp.csv'
 df = pd.read_csv(path)
 
+#Code 为国家缩写，plotly_express 用缩写来识别国家位置
 fig = px.choropleth(df, locations="Code", color="AverageTemperature", hover_name="Country", animation_frame="dt",
 color_continuous_scale=px.colors.sequential.Plasma)
 fig.show()
